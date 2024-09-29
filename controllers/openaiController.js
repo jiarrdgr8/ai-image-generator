094edbc645a48d931128402d4de1e17446a69a6d
Jiarr <114872804+jiarrdgr8@users.noreply.github.com>
2024-09-29 22:41:14 +0800

    switch to dall-e-3

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -5,39 +5,40 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-
 const generateImage = async (req, res) => {
-    const { prompt, size } = req.body
+  const { prompt, size } = req.body;
 
-    const imageSize = size === 'small' ? '256x256' : size === 'medium' ? '512x512' : '1024x1024'
+  // this is for dall-e-2 model
+  //   const imageSize =
+  //     size === "small" ? "256x256" : size === "medium" ? "512x512" : "1024x1024";
 
+  try {
+    const response = await openai.createImage({
+      model: "dall-e-3",
+      prompt,
+      n: 1,
+      size: size,
+    });
 
-    try {
-        const response = await openai.createImage({
-            prompt,
-            n: 1,
-            size: imageSize
-        })
+    const imageUrl = response.data.data[0].url;
 
-        const imageUrl = response.data.data[0].url
-    
-        res.status(200).json({
-            success: true,
-            data: imageUrl
-        })
-    } catch (error) {
-        if (error.response) {
-            console.log(error.response.status);
-            console.log(error.response.data);
-        } else {
-            console.log(error.message);
-        }
-
-        res.status(400).json({
-            success: false,
-            error: 'The image could not be generated.'
-        })
+    res.status(200).json({
+      success: true,
+      data: imageUrl,
+    });
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else {
+      console.log(error.message);
     }
-}
 
-module.exports = { generateImage }
+    res.status(400).json({
+      success: false,
+      error: "The image could not be generated.",
+    });
+  }
+};
+
+module.exports = { generateImage };
